dac994d74aa1c158d026ab76e2cb2dc6a3ee5b72
Wanderley <100Palavr@gmail.com>
2023-04-01 17:28:43 -0300

    Update eventDispatcher.cpp

--- eventDispatcher.cpp
+++ eventDispatcher.cpp
@@ -1,46 +1,38 @@
 #include "eventDispatcher.h"
 
-void Dispatcher::addListener(std::string eventType, Callback callback, double weigth = 0.1) {
+void Dispatcher::addListener(std::string eventType, Callback callback, int weigth = 1) {
     this->listeners.insert({eventType, std::make_pair(callback, weigth)});
 }
 
 // notifica/trigga todos os listeners de um evento específico
 void Dispatcher::notify(Event* e) {
-    auto listeners = this->orderListeners(e->getType());
+    auto queue = this->orderListeners(e->getType());
 
-    for(auto func : listeners) {
+    while(!queue.empty()) {
+        Callback func = queue.top();
         func(e);
+        queue.pop();
     }
 }
 
 void Dispatcher::notify(std::string eventType) {
-    auto listeners = this->orderListeners(eventType);
+    auto queue = this->orderListeners(eventType);
 
-    for(auto func : listeners) {
+    while(!queue.empty()) {
+        Callback func = queue.top();
         func(nullptr);
+        queue.pop();
     }
 }
 
-
-// ordena os listeners por peso
-std::vector<Callback> Dispatcher::orderListeners(std::string eventType) {
-    std::vector<listenerPair> sortedArray;
+std::priority_queue<int, Callback> Dispatcher::orderListeners(std::string eventType) {
+    std::priority_queue<int, Callback> queue;
 
     for(auto& element : this->listeners) {
         if(element.first == eventType) {
-            sortedArray.push_back(element.second);
+            queue.push(element.second);
         }
     }
 
-    sort(sortedArray.begin(), sortedArray.end(), [](listenerPair a, listenerPair b) {
-        return a.second > b.second;
-    });
-
-    std::vector<Callback> res;
-
-    std::for_each(sortedArray.begin(), sortedArray.end(), [&res](listenerPair element) {
-        res.push_back(element.first);
-    });
-
-    return res;
+    return queue;
 }
